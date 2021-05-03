cc93f0b4cea6d69a81ddade0e29704ee472f7fa2
babidev <babidev@protonmail.com>
2021-05-03 11:38:29 +0200

    Add timer test smartptrs

---
+++ timer_test_smartptrs.cpp
@@ -0,0 +1,47 @@
+//g++ -o timer_test_smartptrs timer_test_smartptrs.cpp -O2 -DNDEBUG
+
+#include <array>
+#include <iostream>
+#include <memory>
+#include "timer.h"
+
+struct point {
+  int x;
+  int y;
+};
+
+int main()
+{
+  std::cout << "Mesure the unique_ptr using new" << std::endl;
+  {
+    std::array<std::unique_ptr<point>, 1000> unique_ptrs;
+    utility::timer timer;
+    for (int i = 0; i < unique_ptrs.size(); ++i) {
+      unique_ptrs[i] = std::unique_ptr<point>(new point());
+    }
+  }
+  std::cout << "Mesure the unique_ptr using make_unique" << std::endl;
+  {
+    std::array<std::unique_ptr<point>, 1000> unique_ptrs;
+    utility::timer timer;
+    for (int i = 0; i < unique_ptrs.size(); ++i) {
+      unique_ptrs[i] = std::make_unique<point>();
+    }
+  }
+  std::cout << "Mesure the shared_ptr using new" << std::endl;
+  {
+    std::array<std::shared_ptr<point>, 1000> shared_ptrs;
+    utility::timer timer;
+    for (int i = 0; i < shared_ptrs.size(); ++i) {
+      shared_ptrs[i] = std::shared_ptr<point>(new point());
+    }
+  }
+  std::cout << "Mesure the shared_ptr using make_shared" << std::endl;
+  {
+    std::array<std::shared_ptr<point>, 1000> shared_ptrs;
+    utility::timer timer;
+    for (int i = 0; i < shared_ptrs.size(); ++i) {
+      shared_ptrs[i] = std::make_shared<point>();
+    }
+  }
+}
